1fafb0b62af02fc117b054637de1e93a67d0aec3
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-04-29 12:49:59 +0000

    Install crispy forms, add form for comment posting and view

--- codestar/settings.py
+++ codestar/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
     'blog',
 ]
 
@@ -56,6 +57,8 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 MIDDLEWARE = [
